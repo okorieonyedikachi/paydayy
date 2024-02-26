9bbe9f9559c8a5720f2a6815b2144ca04c496d05
okorieonyedikachi <cosmasdaniella@gmail.com>
2024-02-26 11:46:29 +0100

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "./(components)/HeroSection";
+import BodySection from "./(components)/BodySection";
+
+const page = () => {
+  return (
+    <div>
+      <HeroSection />
+      <BodySection />
+    </div>
+  );
+};
+
+export default page;
